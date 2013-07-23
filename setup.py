8e53c1387e2fe282ffa328b079e3022adad04398
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-07-23 17:24:16 +0300

    Python packaging structure.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+try:
+    README = open('README.rst').read() + '\n\n'
+    README += open('CHANGES.rst').read()
+except:
+    README = None
+
+try:
+    REQUIREMENTS = open('requirements.txt').read()
+except:
+    REQUIREMENTS = None
+
+setup(
+    name='nodegraph',
+    version='0.1',
+    author='Mathijs de Bruin',
+    author_email='mathijs@mathijsfiets.nl',
+    packages=find_packages(),
+    url='https://pypi.python.org/pypi/nodegraph/',
+    description='Perspectivist graph database.',
+    long_description=README,
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
