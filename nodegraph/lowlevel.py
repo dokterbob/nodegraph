149ddd0d27dc30722c4e6c75012a0ca607048f8f
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-08-07 11:30:00 +0200

    Another layer to the onion.

--- nodegraph/lowlevel.py
+++ nodegraph/lowlevel.py
@@ -18,6 +18,26 @@ class Node(object):
 
         self.name = name
 
+    def get_edges_to(self):
+        """ Return set of edges ending at node. """
+        raise NotImplementedError
+
+    def get_edges_from(self):
+        """ Return set of edges starting at node. """
+        raise NotImplementedError
+
+    def get_total_score(self):
+        """ Total score of all edges starting at this node. """
+
+        edges = self.get_edges_from()
+
+        # TODO: Optimize this
+        total_score = 0
+        for edge in edges:
+            total_score += edge.score
+
+        return total_score
+
 
 class Edge(object):
     """
@@ -31,6 +51,9 @@ class Edge(object):
         self.from_node = from_node
         self.to_node = to_node
 
+        # Initialize score
+        self._score = 0
+
     def increase_score(self, amount=100):
         """ Increase the score with the given amount. """
         self._score += amount
@@ -40,9 +63,27 @@ class Edge(object):
         self._score -= amount
 
     @property
+    def graph(self):
+        assert self.from_node.graph is self.to_node.graph
+
+        return self.from_node.graph
+
+    @property
     def score(self):
-        """ Get the current score. """
+        """ Return the current score. """
         return self._score
+
+    @property
+    def weight(self):
+        """ Return the current weight. """
+        total_score = self.from_node.get_total_score()
+
+        assert self._score
+        assert total_score
+
+        weight = self.score / total_score
+
+        return weight
 
 
 class Path(object):
