7a88cee07ba6ac330907a3f6af8335abaea0577a
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-08-07 09:10:30 +0200

    Basic objects.

---
+++ nodegraph/lowlevel.py
@@ -0,0 +1,23 @@
+class Graph(object):
+    """ Container for other objects. """
+    pass
+
+
+class Node(object):
+    """ Node in a graph. """
+    pass
+
+
+class Edge(object):
+    """ Connection between two Nodes. """
+    pass
+
+
+class Path(object):
+    """ Ordered collection of Edges. """
+    pass
+
+
+class Ensemble(object):
+    """ Collection of Paths between two Nodes. """
+    pass
