53e315cddf6bef8871e06243e4ecbed43a640af6
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-08-07 09:21:47 +0200

    Some basic structure for objects.

--- nodegraph/lowlevel.py
+++ nodegraph/lowlevel.py
@@ -1,23 +1,61 @@
 class Graph(object):
     """ Container for other objects. """
-    pass
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Node(object):
-    """ Node in a graph. """
-    pass
+    """
+    Node in a graph.
+    """
+
+    def __init__(self, graph, name):
+        assert isinstance(graph, Graph)
+        self.graph = graph
+
+        self.name = name
 
 
 class Edge(object):
-    """ Connection between two Nodes. """
-    pass
+    """
+    Weighed connection between two Nodes.
+    """
+
+    def __init__(self, from_node, to_node, weight=None):
+        assert isinstance(from_node, Node)
+        assert isinstance(to_node, Node)
+        self.from_node = from_node
+        self.to_node = to_node
+
+        if weight:
+            assert isinstance(weight, float)
+            assert weight >= 0.0
+            assert weight <= 1.0
+            self.weight = weight
+        else:
+            weight = 0.0
 
 
 class Path(object):
-    """ Ordered collection of Edges. """
-    pass
+    """
+    Ordered collection of Edges with compound weight as product of Edge weight.
+    """
+
+    def __init__(self, edges=[]):
+        # TODO: Put this iteration into the assert statement, somehow
+        for edge in edges:
+            assert isinstance(edge, Edge)
+        self.edges = edges
 
 
 class Ensemble(object):
-    """ Collection of Paths between two Nodes. """
-    pass
+    """
+    Collection of Paths between two Nodes with compound weight as sum of path
+    weights.
+    """
+    def __init__(self, paths=set()):
+        # TODO: Put this iteration into the assert statement, somehow
+        for path in paths:
+            assert isinstance(path, Path)
+        self.paths = paths
