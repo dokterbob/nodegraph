f39c0b02a3c2cbf28fcf2732e9058d3a4d7c19f7
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-08-29 15:03:59 +0200

    Implemented Path.get_weight()

--- nodegraph/highlevel.py
+++ nodegraph/highlevel.py
@@ -1,3 +1,4 @@
+import sys
 from .lowlevel import Edge
 
 
@@ -7,6 +8,8 @@ class Path(object):
     """
 
     def __init__(self, edges=[]):
+        assert len(edges) >= 1, 'Paths should contain at least two Edges.'
+
         # This is equivalent to the assert statement, see:
         # http://docs.python.org/2/reference/simple_stmts.html#the-assert-statement
         if __debug__:
@@ -39,6 +42,56 @@ class Path(object):
 
     def __hash__(self):
         return hash(self.key())
+
+    def get_ttl(self):
+        """
+        Returns the path ttl, which is the minimum of all the path's
+        Edge ttls.
+        """
+
+        min_ttl = sys.maxint
+
+        for edge in self.edges:
+            if edge.ttl < min_ttl:
+                min_ttl = edge.ttl
+
+        assert min_ttl != sys.maxint
+
+        return min_ttl
+
+    def get_weight(self):
+        """
+        Returns the weight for this path.
+
+        This is a compound property based on the product of the weight of
+        all the Path's Edges multiplied with the Graph's `path_dampening`
+        factor for each Edge.
+        """
+
+        # Nice hashable tuple of self and identifier
+        cache_key = (self, 'weight')
+
+        # Hit cache
+        cached = self.graph.store.cache.get(cache_key)
+        if cached is not None:
+            return cached
+
+        weight = 1.0
+        for edge in self.edges:
+            weight *= edge.get_weight()
+            print weight
+
+        # Calculate dampening factor for number of steps *between* Edges
+        dampening_factor = self.dampening ** (len(self.edges) - 1)
+        weight *= dampening_factor
+
+        # Get minimal outgoing ttl for Edges
+        ttl = self.get_ttl()
+
+        # Write to cache
+        self.graph.store.cache.set(cache_key, weight, ttl)
+
+        return weight
 
 
 class Ensemble(object):
